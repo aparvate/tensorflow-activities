c88868b893c6ea3eb46aafa62ac84b46142f6117
aparvate <amogh.parvate@gmail.com>
2023-07-25 18:28:36 -0700

    Did chapter 2, activity 1

---
+++ chapter2activity1.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import random
+
+#Generate random data
+np.random.seed(random.randint(0,500000))
+area = 2.5 * np.random.randn(100) + 25
+price = 25 * area + 5 + np.random.randint(20,50, size = len(area))
+data = np.array([area, price])
+data = pd.DataFrame(data = data.T, columns=['area','price'])
+plt.scatter(data['area'], data['price'])
+
+#Regression coeffs
+W = sum(price*(area-np.mean(area))) / sum((area-np.mean(area))**2)
+b = np.mean(price) - W*np.mean(area)
+print("The regression coefficients are", W,b)
+
+#New price prediction
+y_pred = W * area + b
+
+#plot predicted prices
+plt.plot(area, y_pred, color='red',label="Predicted Price")
+plt.scatter(data['area'], data['price'], label="Training Data")
+plt.xlabel("Area")
+plt.ylabel("Price")
+plt.legend()
+plt.show() 
